f7f1f7b99f726ae0f5bcd693ecec543e76f8f5d5
QuasiConnective <39988244+QuasiConnective@users.noreply.github.com>
2018-09-07 10:53:54 +1200

    Update script.js

--- script.js
+++ script.js
@@ -179,7 +179,7 @@ function upd() {
   if (bhmax >= 1e+61) {
     bhmax = 1e+60;
   }
-  pps = t1f*mult*bmult*sps*(2**r1);
+  pps = t1f*mult*bmult*(2**r1);
   if (pps >= 1000) {
     ppsdisp = pps.toExponential(2);
   } else {
@@ -568,24 +568,16 @@ function reset() {
 }
 var sps = 1;
 function prchtd(x){
-  if (x === 0){
-    if (sing >= 5){
-      sing -= 5;
-      document.getElementById("sing").innerHTML = sing;
-      sps += 1;
-      document.getElementById("sps").innerHTML = sps;
-    }
-  }
 }
-setInterval(function(){incr(0.05*t1f*mult*bmult*sps*(2**r1));},50);
+setInterval(function(){incr(0.05*t1f*mult*bmult(2**r1));},50);
 setInterval(function(){upd();},50);
-setInterval(function(){mf1(t2f*mult*sps);},1000);
-setInterval(function(){mf2(t3f*mult*sps);},1000);
-setInterval(function(){mf3(t4f*mult*sps);},1000);
-setInterval(function(){mf4(t5f*mult*sps);},1000);
-setInterval(function(){mf5(t6f*mult*sps);},1000);
-setInterval(function(){mf6(t7f*mult*sps);},1000);
-setInterval(function(){mf7(t8f*mult*sps);},1000);
+setInterval(function(){mf1(t2f*mult);},1000);
+setInterval(function(){mf2(t3f*mult);},1000);
+setInterval(function(){mf3(t4f*mult);},1000);
+setInterval(function(){mf4(t5f*mult);},1000);
+setInterval(function(){mf5(t6f*mult);},1000);
+setInterval(function(){mf6(t7f*mult);},1000);
+setInterval(function(){mf7(t8f*mult);},1000);
 setInterval(function(){upds();},50);
 setInterval(function(){bh();},50);
 setInterval(function(){bhtest();},50);
